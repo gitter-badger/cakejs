3ccac01ec27f823b46613f1d6edfd2850e17cffc
Tiinusen <tiinusen@cakejs.net>
2015-04-14 04:23:56 +0200

    Added CakeJS to global on require

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export var Controller = require('./Controller');
 export var Datasource = require('./Datasource');
 export var ORM = require('./ORM');
+export var Core = require('./Core');
 export * from './CakeJS'
+global.CakeJS = exports;
