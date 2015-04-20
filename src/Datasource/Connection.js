002d17c991c2e6a4c6c541a99ec1422cf8712787
Tiinusen <tiinusen@cakejs.net>
2015-04-21 01:03:59 +0200

    Began datasources

---
+++ src/Datasource/Connection.js
@@ -0,0 +1,8 @@
+//Types
+
+export class Connection{
+	constructor(configuration){
+		this._configuration = configuration;
+		console.log(this._configuration);
+	}
+}
--- src/Datasource/ConnectionManager.js
+++ src/Datasource/ConnectionManager.js
@@ -1,9 +1,23 @@
+//Types
+import {MissingConfigException} from '../Exception/MissingConfigException'
+import {AlreadyDefinedException} from '../Exception/AlreadyDefinedException'
+import {Connection} from './Connection'
+
 export var ConnectionManager = new class {
 	constructor(){
 		this._configurations = {}
+		this._connections = {};
 	}
 	config(name, configuration){
 		if(name in this._configurations)
-			throw new Exception("");
+			throw new AlreadyDefinedException("ConnectionMananger: "+name);
+		this._configurations[name] = configuration;
+	}
+	get(name){
+		if(!(name in this._configurations))
+			throw new MissingConfigException();
+		if(!(name in this._connections))
+			this._connections[name] = new Connection(this._configurations[name]);
+		return this._connections[name];
 	}
 }();
