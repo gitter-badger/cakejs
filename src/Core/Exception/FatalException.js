9e6a9f1664800fbf0fcf361e9f18af7775bcd222
Tiinusen <tiinusen@cakejs.net>
2015-04-22 02:33:00 +0200

    Made FatalException process.exit(1) and show stack

--- src/Core/Exception/FatalException.js
+++ src/Core/Exception/FatalException.js
@@ -4,5 +4,7 @@ import {Exception} from './Exception'
 export class FatalException extends Exception {
 	constructor(message){
 		super(message);
+		console.log(this.stack);
+		process.exit(1);
 	}
 }
