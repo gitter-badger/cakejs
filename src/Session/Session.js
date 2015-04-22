4f8815e2c2af9bf7c4c13593e0c0febdd8baf52a
Tiinusen <tiinusen@cakejs.net>
2015-04-23 00:48:34 +0200

    Replaced for use of collection

--- src/Session/Session.js
+++ src/Session/Session.js
@@ -1,6 +1,7 @@
 //Types
 import {InvalidParameterException} from '../Exception/InvalidParameterException'
 import {ConnectionContainer} from '../WebSocket/ConnectionContainer'
+import {Collection} from '../Collection/Collection'
 
 //Function
 import uuid from '../Utilities/uuid'
@@ -8,82 +9,46 @@ import uuid from '../Utilities/uuid'
 class SessionData {
 	constructor(session){
 		this.__session = session;
-		this.__data = {};
+		this.__collection = new Collection();
 	}
-	read(key){
-		if(typeof key !== 'string' || key.trim() === '')
-			throw new InvalidParameterException(key, 'string');
-		var list = key.split(".");
-		var cursor = this.__data;
-		while(list.length > 0){
-			var listItem = list.shift();
-			if(typeof cursor !== 'object')
-				return null;
-			if(!(listItem in cursor))
-				return null;
-			cursor = cursor[listItem];
-		}
-		return cursor;
+	read(keyPath){
+		if(typeof keyPath !== 'string' || keyPath.trim() === '')
+			throw new InvalidParameterException(keyPath, 'string');
+		var value = this.__collection.extract(keyPath);
+		if(typeof value === 'object' && value instanceof Collection)
+			return value.toObject(true);
+		return value;
 	}
-	write(key, value){
-		if(typeof key !== 'string' || key.trim() === '')
-			throw new InvalidParameterException(key, 'string');
+	write(keyPath, value){
+		if(typeof keyPath !== 'string' || keyPath.trim() === '')
+			throw new InvalidParameterException(keyPath, 'string');
 		if(typeof value === 'undefined')
 			throw new InvalidParameterException(value, 'anything');
-		var list = key.split(".");
-		var cursor = this.__data;
-		while(list.length > 1){
-			var listItem = list.shift();
-			if(typeof cursor !== 'object')
-				return false;
-			if(!(listItem in cursor))
-				cursor[listItem] = {};
-			cursor = cursor[listItem];
-		}
-		var listItem = list.shift();
-		cursor[listItem] = value;
+		this.__collection.insert(keyPath, value);
 		return true;
 	}
-	delete(key){
-		if(typeof key !== 'string' || key.trim() === '')
-			throw new InvalidParameterException(key, 'string');
-		var list = key.split(".");
-		var cursor = this.__data;
-		while(list.length > 1){
-			var listItem = list.shift();
-			if(typeof cursor !== 'object')
-				return false;
-			if(!(listItem in cursor))
-				return false;
-			cursor = cursor[listItem];
-		}
-		var listItem = list.shift();
-		delete cursor[listItem];
-		return true;
+	delete(keyPath){
+		if(typeof keyPath !== 'string' || keyPath.trim() === '')
+			throw new InvalidParameterException(keyPath, 'string');
+		this.__collection = this.__collection.reject((value, innerKey) => {
+			return keyPath === innerKey;
+		});
 	}
-	consume(key){
-		var value = this.read(key);
-		this.delete(key);
+	consume(keyPath){
+		if(typeof keyPath !== 'string' || keyPath.trim() === '')
+			throw new InvalidParameterException(keyPath, 'string');
+		var value = this.read(keyPath);
+		this.delete(keyPath);
 		return value;
 	}
 	check(key){
-		if(typeof key !== 'string' || key.trim() === '')
-			throw new InvalidParameterException(key, 'string');
-		var list = key.split(".");
-		var cursor = this.__data;
-		while(list.length > 0){
-			var listItem = list.shift();
-			if(typeof cursor !== 'object')
-				return false;
-			if(!(listItem in cursor))
-				return false;
-			cursor = cursor[listItem];
-		}
-		return true;
+		if(typeof keyPath !== 'string' || keyPath.trim() === '')
+			throw new InvalidParameterException(keyPath, 'string');
+		return this.read(key) !== null;
 	}
 	destroy(){
 		//Not yet implemented properly
-		this.__data = {};
+		this.__collection = new Collection();
 	}
 	renew(){
 		this.__session.touch();
