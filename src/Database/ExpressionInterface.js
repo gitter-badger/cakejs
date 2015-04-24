a6a91596d06373a9407a79d25bc63b8bb8065958
Tiinusen <tiinusen@cakejs.net>
2015-04-24 02:12:59 +0200

    Ported ExpressionInterface

---
+++ src/Database/ExpressionInterface.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2015 Tiinusen
+ * 
+ * Many thanks to Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * This was inspired by http://cakephp.org CakePHP(tm) Project
+ * 
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * @copyright   Copyright (c) 2015 Tiinusen
+ * @link        https://github.com/cakejsframework/cakejs
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * This class is a port of CakePHP 3.0
+ * https://github.com/cakephp/cakephp/blob/master/src/Database/ExpressionInterface.php
+ */
+
+//CakeJS.Database.ExpressionInterface
+
+export class ExpressionInterface{
+	sql(){throw new NotImplementedException();}
+	traverse(){throw new NotImplementedException();}
+}
