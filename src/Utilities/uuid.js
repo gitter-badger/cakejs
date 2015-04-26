4248b71703b4c50eb14053828b8dac54171d252f
Tiinusen <tiinusen@cakejs.net>
2015-04-26 23:02:05 +0200

    Added a offset

--- src/Utilities/uuid.js
+++ src/Utilities/uuid.js
@@ -24,14 +24,15 @@ function replaceAt(str, index, character) {
     return str.substr(0, index) + character + str.substr(index+character.length);
 }
 
-export default function uuid(string, prefix, prefixLength){
+export default function uuid(string, prefix, prefixLength, offset){
 	string = md5(typeof string === 'string' ? string : new Date().getTime()+Math.random());
+	offset = typeof offset === 'number' ? offset : 9;
 	string = string.substr(0,8)+"-"+string.substr(0+8,4)+"-"+string.substr(0+8+4,4)+"-"+string.substr(0+8+4+4,4)+"-"+string.substr(0+8+4+4+4,12);
 	if(typeof prefix === 'string'){
 		prefixLength = typeof prefixLength === 'undefined' ? 4 : prefixLength;
 		prefix = md5(prefix);
 		prefix = prefix.substr(0,8)+"-"+prefix.substr(0+8,4)+"-"+prefix.substr(0+8+4,4)+"-"+prefix.substr(0+8+4+4,4)+"-"+prefix.substr(0+8+4+4+4,12);
-		for(var i = 0; i < prefixLength; i++)
+		for(var i = offset; i < prefixLength+offset; i++)
 			string = replaceAt(string,i,prefix[i]);
 	}
 	return string;
