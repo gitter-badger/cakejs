3ae498bdc1de90f92241b2498840c1862fa65e26
Tiinusen <tiinusen@cakejs.net>
2015-04-26 19:06:47 +0200

    Changed isEmpty

--- src/Utilities/isEmpty.js
+++ src/Utilities/isEmpty.js
@@ -18,11 +18,32 @@
 //CakeJS.Utilities.isEmpty
 
 export default function isEmpty(obj){
-	if (obj === null) return true;
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-    return true;
+	switch(typeof obj){
+		case "undefined":
+			return true;
+		case "number":
+			return obj === 0;
+			break;
+		case "string":
+			return obj.length === 0;
+			break;
+		case "boolean":
+			return obj === false;
+			break;
+		case "object":
+			if(obj === null)
+				return true;
+			if(obj instanceof Array)
+				return obj.length === 0;
+			if('count' in obj)
+				return obj.count === 0;
+			if('length' in obj)
+				return obj.length === 0;
+			for (var key in obj) {
+				if (hasOwnProperty.call(obj, key)) return false;
+			}
+			break;
+		default:
+			throw new Error("Bad");
+	}
 }
