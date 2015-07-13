5801f14049f0440e5bd7553dcc355edc69733b89
Tiinusen <tiinusen@cakejs.net>
2015-07-13 17:47:08 +0200

    Began rewriting AppTest

--- tests/TestCase/AppTests.js
+++ tests/TestCase/AppTests.js
@@ -20,159 +20,56 @@ var filename = path.basename(__filename);
 //Uses
 var Configure = CakeJS.Core.Configure;
 var ClassLoader = CakeJS.Core.ClassLoader;
-var TestCase = CakeJS.TestSuite.TestCase;
+var IntegrationTestCase = CakeJS.TestSuite.IntegrationTestCase;
+var Client = CakeJS.Network.Http.Client;
 
-var test = new class AppTests extends TestCase
+var test = new class AppTests extends IntegrationTestCase
 {
-	
-	
-	testClassloader()
+	/**
+	 * Tests ClassLoader 
+	 */
+	testClassLoader()
 	{
+		ClassLoader.loadClass('Controller', 'Controller');
+		ClassLoader.loadClass('TestController', 'Controller');
+		ClassLoader.loadClass('TestPlugin.MyController', 'Controller');
 		ClassLoader.loadFolder('Controller');
-		return true;
 	}
-	async testServer_Create() {
-		//var result = await CakeJS.Core.ClassLoader.load(path.resolve(__filename,"..","src/test.js"));
-		this._server = CakeJS.createServer();
-				
-		var result = await CakeJS.ORM.TableRegistry
-				.get("table")
-				.find('all')
-				.all();
-		result.each((row) => {
-			console.log(row);
-		});
+	
+	/**
+	 * Tests CakeJS.createServer()
+	 */
+	async testServer_Create() 
+	{		
+		this._server = await CakeJS.createServer();
 	}
-	/*async server_create(){
-		this._server = CakeJS.createServer();
-		this._server.config({
-			"Listen": {
-				"port": 31337
-			},
-			"CakeJS": {
-				"app": path.resolve(__filename,"..","src"),
-			},
-			"Static": {
-				"webroot": path.resolve(__filename,"..","webroot"),
-			},
-			"Datasources": {
-				"default": {
-					"driver": "Mysql",
-					"host": "127.0.0.1",
-					"username": "test",
-					"password": "test",
-					"database": "test"
-				}
-			}
-		});
+	
+	/**
+	 * Tests start of server
+	 */
+	async testServer_Start()
+	{
 		await this._server.start();
 	}
-	async connection(){
-		var response = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/').on('error', error => {return reject(error);}).on('response',response => {
+	
+	/**
+	 * Tests connection to see if Express is active
+	 * and can serve static content
+	 */
+	async testServer_Connection(){
+		await this.get('/');
+		this.assertResponseOk();
+		//var [url, query] = CakeJS.Routing.Router.url('/tiinusen/?hejsan=titta', true);
+		//console.log(url);
+		//console.log(query);
+		/*var response = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/js/client.js').on('error', error => {return reject(error);}).on('response',response => {
 			resolve(response);
 		}));
 		assert.equal(response.statusCode, 200, "Was expecting a successful get");
 		var data = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/').on('error', error => {return reject(error);}).on('data',data => {
 			resolve(data);
 		})); 
-		assert.equal(data.toString(), fs.readFileSync(path.resolve(__filename,"..","webroot", "index.html")), "The response was incorrect");
+		assert.equal(data.toString(), fs.readFileSync(path.resolve(WWW_ROOT, "index.html")), "The response was incorrect");*/
 	}
-	async routing(){
-		var response = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/Test').on('error', error => {return reject(error);}).on('response',response => {
-			resolve(response);
-		}));
-		assert.equal(response.statusCode, 200, "Was expecting a successful get");
-		var data = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/Test').on('error', error => {return reject(error);}).on('data',data => {
-			resolve(data);
-		}));
-		try{
-			data = JSON.parse(data);
-		}catch(e){
-			throw new Error("Unable to parse data");
-		}
-		assert.equal(data, true, "The response was incorrect");
-	}
-	async routing_error_regular(){
-		var response = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/Test/error').on('error', error => {return reject(error);}).on('response',response => {
-			resolve(response);
-		}));
-		assert.equal(response.statusCode, 500, "Was expecting a failure get");
-	}
-	async routing_error_client(){
-		var response = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/Test/client_error').on('error', error => {return reject(error);}).on('response',response => {
-			resolve(response);
-		}));
-		assert.equal(response.statusCode, 520, "Was expecting a failure get");
-		var data = await new Promise((resolve, reject) => require('request').get('http://127.0.0.1:31337/Test/client_error').on('error', error => {return reject(error);}).on('data',data => {
-			resolve(data);
-		}));
-		assert.equal(data.toString(),'{"custom":"error"}', "Was expecting a json string");
-	}
-	async routing_post(){
-		var form = {
-			"keyA": "valueA",
-			"keyB": "valueB"
-		};
-		var response = await new Promise((resolve, reject) => require('request').post('http://127.0.0.1:31337/Test/post').form(form).on('error', error => {return reject(error);}).on('response',response => {
-			resolve(response);
-		}));
-		assert.equal(response.statusCode, 200, "Was expecting a successful get");
-		var data = await new Promise((resolve, reject) => require('request').post('http://127.0.0.1:31337/Test/post').form(form).on('error', error => {return reject(error);}).on('data',data => {
-			resolve(data);
-		}));
-		try{
-			data = JSON.parse(data);
-		}catch(e){
-			throw new Error("Unable to parse data");
-		}
-		assert.equal(JSON.stringify(data), JSON.stringify(form), "The response was incorrect");
-	}
-	async orm_sql(){
-		var sql = null;
-		//Tests a Select
-		sql = await CakeJS.ORM.TableRegistry
-				.get("table")
-				.find()
-				.select('column_a', 'column_b')
-				.where({'column_c': 50, 'column_d': 50})
-				.sql();
-		assert.equal(sql, 'SELECT tables.column_a AS `tables__column_a` FROM tables WHERE (column_c = :0 AND column_d = :1)', 'Was expecting a proper sql query');
-		
-		//Tests a Insert
-		sql = await CakeJS.ORM.TableRegistry
-				.get("table")
-				.query()
-				.insert(['id', 'column_a', 'column_b'])
-				.values({'id': 2,'column_a': 50, 'column_b': 50})
-				.sql();
-		assert.equal(sql, 'INSERT INTO tables (id, column_a, column_b) VALUES (:0, :1, :2)', 'Was expecting a proper sql query');
-		
-		//Tests a Update
-		sql = await CakeJS.ORM.TableRegistry
-				.get("table")
-				.query()
-				.update()
-				.set({'column_c': 50, 'column_d': 50})
-				.where({'column_c': 70, 'column_d': 100})
-				.sql();
-		assert.equal(sql, 'UPDATE tables SET column_c = :0 , column_d = :1 WHERE (column_c = :2 AND column_d = :3)', 'Was expecting a proper sql query');
-		
-		//Tests a Delete
-		sql = await CakeJS.ORM.TableRegistry
-				.get("table")
-				.query()
-				.delete()
-				.where({'column_c': 70, 'column_d': 100})
-				.sql();
-		assert.equal(sql, 'DELETE FROM tables WHERE (column_c = :0 AND column_d = :1)', 'Was expecting a proper sql query');
-	}
-	async process_init(){
-		var process = CakeJS.Process.ProcessManager.get('Test');
-		assert.equal(process.keyA, 'valueA', 'Expected valueA in process.keyA');
-	}
-	async process_start(){
-		var process = CakeJS.Process.ProcessManager.get('Test');
-		assert.equal(process.keyB, 'valueB', 'Expected valueB in process.keyB');
-	}*/
 }
 module.exports = test.moduleExports();
