63fe3cd554e2e1aa5fe0f653951cdf9fb4da2a86
Tiinusen <tiinusen@cakejs.net>
2015-07-13 12:48:10 +0200

    Added QueryTest and added existing but modified Query tests

---
+++ tests/TestCase/ORM/QueryTest.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2015 Tiinusen
+ * 
+ * Many thanks to Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * This was inspired by http://cakephp.org CakePHP(tm) Project
+ * 
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * @copyright   Copyright (c) 2015 Tiinusen
+ * @link        https://github.com/cakejsframework/cakejs
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+//Uses
+var TestCase = CakeJS.TestSuite.TestCase;
+
+var test = new class QueryTest extends TestCase
+{
+	/**
+	 * Tests QueryBuilder constructs correct SQL query
+	 * when selecting all
+	 */
+	testSelect_All()
+	{
+		var sqlQuery = CakeJS.ORM.TableRegistry
+			.get("table")
+			.find('all')
+			.sql();
+		this.assertEquals(sqlQuery, "SELECT * FROM tables");
+	}
+	
+	/**
+	 * Tests QueryBuilder constructs correct SQL query
+	 * when selecting columnA and columnB
+	 */
+	testSelect_Specific()
+	{
+		var sqlQuery = CakeJS.ORM.TableRegistry
+			.get("table")
+			.find()
+			.select(['columnA', 'columnB'])
+			.sql();
+		this.assertEquals(sqlQuery, "SELECT tables.columnA AS `tables__columnA`, tables.columnB AS `tables__columnB` FROM tables");
+	}
+	
+	/**
+	 * Tests QueryBuilder constructs correct SQL query
+	 * when performing a indexless insert
+	 */
+	testInsert()
+	{
+		var sqlQuery = CakeJS.ORM.TableRegistry
+			.get("table")
+			.query()
+			.insert(['columnA', 'columnB'])
+			.values({
+				'columnA': 'valueA',
+				'columnB': 'valueB'
+			})
+			.sql();
+		this.assertEquals(sqlQuery, "INSERT INTO tables (columnA, columnB, id) VALUES (:0, :1, :2)");
+	}
+	
+	/**
+	 * Tests QueryBuilder constructs correct SQL query
+	 * when performing a update with set and where statements
+	 */
+	testUpdate()
+	{
+		var sqlQuery = CakeJS.ORM.TableRegistry
+			.get("table")
+			.query()
+			.update()
+			.set({'columnA': 'newValueA'})
+			.where({'columnB': 'valueB'})
+			.sql();
+		this.assertEquals(sqlQuery, "UPDATE tables SET columnA = :0 WHERE (columnB = :1)");
+	}
+	
+	/**
+	 * Tests QueryBuilder constructs correct SQL query
+	 * when performing a delete with a where statement
+	 */
+	testDelete()
+	{
+		var sqlQuery = CakeJS.ORM.TableRegistry
+			.get("table")
+			.query()
+			.delete()
+			.where({'columnB': 'valueB'})
+			.sql();
+		this.assertEquals(sqlQuery, "DELETE FROM tables WHERE (columnB = :0)");
+	}
+}
+module.exports = test.moduleExports();
