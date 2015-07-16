45ae3d61cfcd0a673affb2126dcdf2e7f7441725
Andreas Sjöberg <andreas.sjoberg@teleservice.net>
2015-07-16 15:14:03 +0200

    Fixed so Mocha tests can run.

--- bin/cake.js
+++ bin/cake.js
@@ -1,12 +1,22 @@
-var mocha = require('mocha');
+var Mocha = require('mocha');
+var fs = require('fs');
+var path = require('path');
 
 class CakeConsole
-{
+{	
 	/**
 	 * 
 	 */
 	constructor(args)
-	{				
+	{			
+		this.about();
+		
+		this.mocha = new Mocha({
+			'ui': 'exports',
+			'bail': true,
+			'timeout': 5000,
+			'slow': 300
+		});
 		this.bootstrapper = '';
 
 		if (args.length > 0)
@@ -17,63 +27,90 @@ class CakeConsole
 			process.exit(0);
 		}
 		
-		if (!require('fs').existsSync(this.bootstrapper)) {
+		if (!fs.existsSync(this.bootstrapper)) {
 			console.log('Bootstrap could not be loaded.');
 			console.log(this.bootstrapper);
 			process.exit(0);
 		}
 		
+		
 		var bs = require('./' + this.bootstrapper);
-				
-		if (args.indexOf('--test') !== -1) {
-			this.runTest();
+		
+		if (CORE_PATH === undefined || CORE_PATH === null) {
+			console.log('CORE_PATH not set');
+		} else {
+			if (args.indexOf('--constants') !== -1) {
+				displayCakeConstants();
+			}
+			if (args.indexOf('--test') !== -1) {
+				this.runTest();
+			}
 		}
+		
+		console.log('Bye, bye!');
 	}
 	
 	/**
 	 * 
 	 */
-	runMochaTests() {
+	about()
+	{
+		console.log('CakeJS console');
+		console.log('');
+	}
+	/**
+	 * 
+	 */
+	addMochaTests(dir, level) 
+	{
+		fs.readdirSync(dir).forEach((file) => {
+			if (file === 'Model' || file === 'Entity')
+				return;
+			
+			file = path.join(dir, file);
+			var stat = fs.lstatSync(file);
+			if (stat.isFile()) {
+				if (file.substr(-3) === '.js' && file.substr(-12) !== 'bootstrap.js') {
+					console.log('Adding "' + file + '" to Mocha...');
+					this.mocha.addFile(file);
+				}				
+			} else if (stat.isDirectory()) {
+				this.addMochaTests(file, level + 1);
+			};
+		});
+	}
+	
+	runMochaTests()
+	{
+		console.log('Preparing MOCHA tests');
+		this.addMochaTests(CORE_PATH + '/dist/tests', 0);
+		
+		console.log('Running MOCHA tests');
+		this.mocha.run((failures) => {
+			process.exit(failures);
+		});		
 	}
 	
 	/**
 	 * 
 	 */
 	runTest() {
-		var exec = require('child_process').exec;
+		var exec = require('child_process').execSync;
 		
 		var script = [];
-				
-		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'rm -r ' + ROOT + '/dist/tests;'});
-		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'mkdir ' + ROOT + '/dist/tests;'});
-		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'cp -R ' + ROOT + '/tests/* ' + ROOT + '/dist/tests;'});
-		script.push({'stdout': false, 'description': '* Compiling 1/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + ROOT + '/dist/tests/TestCase ' + ROOT + '/dist/tests/TestCase > /dev/null'});
-		script.push({'stdout': false, 'description': '* Compiling 2/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + ROOT + '/dist/tests/test_app/TestApp ' + ROOT + '/dist/tests/test_app/TestApp > /dev/null'});
-		script.push({'stdout': false, 'description': '* Compiling 3/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + ROOT + '/dist/tests/test_app/Plugin/TestPlugin/src ' + ROOT + '/dist/tests/test_app/Plugin/TestPlugin/src > /dev/null'});
-		script.push({'stdout': false, 'description': '* Compiling 4/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + ROOT + '/dist/tests/TestCase ' + ROOT + '/dist/tests/TestCase > /dev/null'});
-		script.push({'stdout': true,  'description': '* Running tests, please wait...', 'src': 'mocha --bail --recursive --slow 300 --timeout 5000 --ui exports -r ' + ROOT + '/dist/tests/bootstrap.js ' + ROOT + '/dist/tests/TestCase'});
-//		script.push({'stdout': true,  'description': '* Running tests, please wait...', 'src': this.runMochaTests });
-		
-		var step = 0;
-		function execute() {
-			if (step < script.length) {
-				console.log(script[step].description);
-				if (typeof script[step].src === 'string') {
-					exec(script[step].src, function callback(error, stdout, stderr) {
-						if (script[step].stdout === true)
-							console.log(stdout);
-
-						step++;
-						if (step < script.length)
-							execute();
-					});			
-				} else if (typeof script[step].src === 'function') {
-					script[step].src();
-				}
-			}
+		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'rm -r ' + CORE_PATH + '/dist/tests'});
+		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'mkdir ' + CORE_PATH + '/dist/tests'});
+		script.push({'stdout': false, 'description': '* Preparing project', 'src': 'cp -R ' + CORE_PATH + '/tests/* ' + CORE_PATH + '/dist/tests'});
+		script.push({'stdout': false, 'description': '* Compiling 1/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + CORE_PATH + '/dist/tests/TestCase ' + CORE_PATH + '/dist/tests/TestCase > /dev/null'});
+		script.push({'stdout': false, 'description': '* Compiling 2/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + CORE_PATH + '/dist/tests/test_app/TestApp ' + CORE_PATH + '/dist/tests/test_app/TestApp > /dev/null'});
+		script.push({'stdout': false, 'description': '* Compiling 3/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + CORE_PATH + '/dist/tests/test_app/Plugin/TestPlugin/src ' + CORE_PATH + '/dist/tests/test_app/Plugin/TestPlugin/src > /dev/null'});
+		script.push({'stdout': false, 'description': '* Compiling 4/4', 'src': 'babel --stage 0 --optional runtime --out-dir ' + CORE_PATH + '/dist/tests/TestCase ' + CORE_PATH + '/dist/tests/TestCase > /dev/null'});
+		for (var i = 0; i < script.length; i++) {
+			console.log(script[i].description);
+			exec(script[i].src);
 		}
-		
-		execute();
+
+		this.runMochaTests();
 	}
 	/**
 	 * 
@@ -85,6 +122,5 @@ class CakeConsole
 	}
 }
 
-console.log(process.argv);
 var cc = new CakeConsole(process.argv.slice(2));
 
