75f12ac8b6639b28ffbc368343b2cc209712309a
Andreas Sjöberg <andreas.sjoberg@teleservice.net>
2015-07-28 17:28:26 +0200

    Class to handle Shell connections.

---
+++ src/Shell/ShellConnection.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import {Client} from '../Network/Net/Client';
+
+export class ShellConnection
+{
+	_client = null;
+	
+	/**
+	 * 
+	 */
+	constructor(connection)
+	{
+		this._client = new Client(connection);
+		this.initialize();
+	}
+	
+	/**
+	 * 
+	 */
+	async initialize()
+	{
+		try
+		{
+			let data = await this._client.read();
+			console.log(data);
+		}
+		catch (e)
+		{
+			console.log(e);
+			await this.client.disconnect();
+		}
+	}
+}
+
