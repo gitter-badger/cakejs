d6180189795db2edd6000b9d8044d4bf5e3857e6
Tiinusen <tiinusen@cakejs.net>
2015-08-10 03:57:08 +0200

    Re added test

--- tests/Fixture/TypesFixture.js
+++ tests/Fixture/TypesFixture.js
@@ -31,10 +31,16 @@ export class TypesFixture extends TestFixture
 
 	records = [
 		{
-			id: Text.uuid(),
+			id: 'e4e5c2f9-1d8c-e8cd-410b-4b906b1bae8e',
 			date: '2014-04-18',
 			time: '15:00:00',
 			datetime: '2014-04-18 15:00:00'
+		},
+		{
+			id: 'e4e5c2f9-1d8c-e8cd-410b-4b906b1bae8f',
+			date: new Date().format("yyyy-mm-dd"), // @todo Shall be adjusted to date, after mysql switch
+			time: new Date().format("HH:MM:ss"),
+			datetime: new Date().format("yyyy-mm-dd HH:MM:ss")
 		}
 	];
 }
